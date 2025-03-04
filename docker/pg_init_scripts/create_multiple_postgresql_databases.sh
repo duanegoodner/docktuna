0085428160226259f0adac9fca36e2696968fe46
Duane Goodner <dmgoodner@gmail.com>
2025-03-03 20:03:03 -0700

    remove non-password secretes

--- docker/pg_init_scripts/create_multiple_postgresql_databases.sh
+++ docker/pg_init_scripts/create_multiple_postgresql_databases.sh
@@ -3,16 +3,6 @@
 set -e
 set -u
 
-#function create_user_and_database() {
-#	local database=$1
-#	echo "  Creating user and database '$database'"
-#	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-#	    CREATE USER $database;
-#	    CREATE DATABASE $database;
-#	    GRANT ALL PRIVILEGES ON DATABASE $database TO $database;
-#EOSQL
-#}
-
 function create_user() {
 	local new_user=$1
 	local password_file=$2
@@ -50,5 +40,4 @@ EOSQL
 create_user "$TUNING_DBS_USER" "$TUNING_DBS_PASSWORD_FILE"
 create_database "$MODEL_TUNING_DB_NAME"
 grant_privileges "$MODEL_TUNING_DB_NAME" "$TUNING_DBS_USER"
-# create_database "$ATTACK_TUNING_DB_NAME"
-# grant_privileges "$ATTACK_TUNING_DB_NAME" "$TUNING_DBS_USER"
+
